1306db5568a7744e88ee619e7d2c21aac8d443e6
Mira <154324598+Miraamal@users.noreply.github.com>
2024-02-15 19:27:44 +0100

    Add files via upload

---
+++ hw-9/script.js
@@ -0,0 +1,25 @@
+// function changeSquare() {
+//     const square = document.getElementById('square');
+//     square.style.backgroundColor = 'green';
+//     square.style.width = '100px';
+//     square.style.height = '100px';
+//   }
+
+// function createParagraph() {
+//     const paragraph = document.createElement('p');
+//     paragraph.textContent = 'Это произвольный текст';
+//     paragraph.style.color = 'blue';
+//     const rootElement = document.querySelector('.root');
+//     rootElement.appendChild(paragraph);
+//   }
+
+// let isBlue = true; 
+
+//   function createParagraph() {
+//     const paragraph = document.createElement('p');
+//     paragraph.textContent = 'Это произвольный текст';
+//     paragraph.style.color = isBlue ? 'blue' : 'green';
+//     const rootElement = document.querySelector('.root');
+//     rootElement.appendChild(paragraph);
+//     isBlue = !isBlue;
+//   }
